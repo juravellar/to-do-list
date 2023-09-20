427c434dd5cf3dea66feb9f48108ea52336431bc
juravellar <julliaravellar@gmail.com>
2023-09-20 00:15:41 -0300

    Correção

--- src/test/resources/import.sql
+++ src/test/resources/import.sql
@@ -1,2 +1,2 @@
-INSERT INTO place (id, name, slug, description, done, preference)
+INSERT INTO place (id, name, description, prioritized, realized, created_at, updated_at)
   VALUES (default, 'Organizar', 'Organizar Armario', 'false', 'false', '1970-01-01 00:00:01', '1970-01-01 00:00:01');
