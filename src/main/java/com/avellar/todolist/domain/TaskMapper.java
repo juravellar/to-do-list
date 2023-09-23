5922ac8b912c96b075f90672404dd525ecb782c1
juravellar <julliaravellar@gmail.com>
2023-09-22 22:49:36 -0300

    correção

--- src/main/java/com/avellar/todolist/domain/TaskMapper.java
+++ src/main/java/com/avellar/todolist/domain/TaskMapper.java
@@ -8,8 +8,8 @@ public class TaskMapper {
     public static Task updateTaskFromDTO(TaskRequest taskRequest, Task task) {
         final String name = StringUtils.hasText(taskRequest.name()) ? taskRequest.name() : task.name();
         final String description = StringUtils.hasText(taskRequest.description()) ? taskRequest.description() : task.description();
-        final Boolean prioritized = taskRequest.prioritized() ? taskRequest.prioritized() : task.prioritized();
         final Boolean realized = taskRequest.realized() ? taskRequest.realized() : task.realized();
+        final Boolean prioritized = taskRequest.prioritized() ? taskRequest.prioritized() : task.prioritized();
         return new Task(task.id(), name, description, prioritized, realized, task.createdAt(), task.updatedAt());
     }
 
