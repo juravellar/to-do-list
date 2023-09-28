f63a24df4b45313150945f6b7ea264e8c12b79e6
juravellar <julliaravellar@gmail.com>
2023-09-28 11:43:38 -0300

    Correção

--- src/main/java/com/avellar/todolist/web/TaskController.java
+++ src/main/java/com/avellar/todolist/web/TaskController.java
@@ -13,7 +13,7 @@ import reactor.core.publisher.Flux;
 import reactor.core.publisher.Mono;
 
 @RestController
-@RequestMapping("/places")
+@RequestMapping("/tasks")
 public class TaskController {
     @Autowired
     private TaskService taskService;
