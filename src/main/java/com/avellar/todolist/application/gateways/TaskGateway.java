072a0dc6ffd889e8f40a00dc034f314bc55b7e50
José Luiz <luizbsilva@gmail.com>
2024-01-09 16:05:36 -0300

    ajustes

--- src/main/java/com/avellar/todolist/application/gateways/TaskGateway.java
+++
@@ -1,7 +0,0 @@
-package com.avellar.todolist.application.gateways;
-
-import com.avellar.todolist.domain.entity.Task;
-
-public interface TaskGateway {
-    Task createTask (Task task);
-}
--- src/main/java/com/avellar/todolist/domain/entity/Task.java
+++
@@ -1,5 +0,0 @@
-package com.avellar.todolist.domain.entity;
-
-public record Task (Long id, String name, String description, Boolean realized, Boolean prioritized) {
-
-}
